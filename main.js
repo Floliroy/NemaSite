7e9c3d904bc87ccd3ae5362127e3b865257f098a
Flo Leeroy <florian.leeroy@hotmail.com>
2022-01-19 20:22:25 +0100

    add youtube videos

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
+require('dotenv').config()
 /**
  * My libraries
  */
+const YouTube = require('youtube-node')
+const youtube = new YouTube()
+youtube.setKey(process.env.YOUTUBE_TOKEN)
+
 const express = require('express')
 const app = express()
  
+/**
+ * Get youtube video IDs
+ */
+let videoIds = new Array()
+function updateVideoIds(){
+    youtube.getPlayListsItemsById("UUHgSUqAOaNpwxVZxd3ZlAZA", 4, function(err, data){
+        if(data.items[0].contentDetails.videoId != videoIds[0]){
+            console.log("Update Youtube videos displayed !")
+        }
+        videoIds = new Array()
+        for(const item of data.items){
+            videoIds.push(item.contentDetails.videoId)
+        }
+    })
+    //re call each 30 minutes
+    setTimeout(updateVideoIds, 30 * 60 * 1000)
+}
+updateVideoIds()
+
 /**
  * Website Init
  */
@@ -15,14 +39,14 @@ app.use(express.json())
 app.get("/", async function(req, res){
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
     console.log(`${ip} asked for index !`);
-    res.render("partials/layout", {body: "index", req: req})
+    res.render("partials/layout", {body: "index", videoIds: videoIds, req: req})
 })
 
 /**
  * Lancement serveur web
  */
 app.use(function (req, res){
-    return res.sendStatus(404)
+    return res.redirect("/")
 })
 app.listen(5000, function(){
     console.log("Server running on port 5000!")
