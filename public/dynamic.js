3b9241e38ba1afd8739d8ea634e7870d9307e05c
Flo Leeroy <florian.leeroy@hotmail.com>
2022-01-24 10:24:22 +0100

    Update dynamic.js

--- public/dynamic.js
+++ public/dynamic.js
@@ -1,3 +1,10 @@
-$(document).on('turbolinks:load', function() { 
-    $('[data-bs-toggle="tooltip"]').tooltip()
+document.addEventListener('turbolinks:load', function() {
+    const dropdown_buttons = document.querySelectorAll('[data-bs-toggle="dropdown"]')
+
+    dropdown_buttons.forEach(function(element) {
+        element.addEventListener('click', function(event) {
+            event.preventDefault()
+            $(this).toggle()
+        })
+    })
 })
